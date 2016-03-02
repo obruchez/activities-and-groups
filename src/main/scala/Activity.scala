c9eb86e4e0a1f07495ab1a9afb8f43a6dd441b10
Olivier Bruchez <olivier@bruchez.org>
2016-03-02 22:19:28 +0100

    Implement some methods

--- src/main/scala/Activity.scala
+++ src/main/scala/Activity.scala
@@ -1,3 +1,5 @@
+import scala.util.Random
+
 case class Activity(name: String, durationInMinutes: Int, costIfAtSameTime: Int)
 
 object Activity {
@@ -8,8 +10,21 @@ object Activity {
     Activity(name = "Jeu société", durationInMinutes = 45, costIfAtSameTime = 1),
     Activity(name = "Jeu adresse", durationInMinutes = 45, costIfAtSameTime = 2))
 
-  def randomOrder(activities: Seq[Activity]): Seq[Activity] = {
-    // @todo
-    Seq()
+  def randomOrder(activities: Seq[Activity]): Seq[Activity] =
+    Random.shuffle(activities)
+
+  def activitiesWithPause(activities: Seq[Activity], pauseInMinutesBetweenActivities: Int): Seq[Activity] = {
+    val pauseActivity = Activity(
+      name = "Pause",
+      durationInMinutes = pauseInMinutesBetweenActivities,
+      costIfAtSameTime = 0)
+
+    val lastActivityIndex = activities.size - 1
+
+    for {
+      (activity, index) <- activities.zipWithIndex
+      lastActivity = index == lastActivityIndex
+      activityWithPause <- if (lastActivity) Seq(activity) else Seq(activity, pauseActivity)
+    } yield activityWithPause
   }
 }
--- src/main/scala/Candidate.scala
+++ src/main/scala/Candidate.scala
@@ -7,14 +7,17 @@ case class Candidate(activitiesByGroup: Map[Group, Seq[Activity]]) {
   }
 
   def asString(startTime: LocalTime): String = {
+    val groupStrings =
+      for ((group, activities) <- activitiesByGroup.toSeq.sortBy(_._1.name)) yield {
+        // @todo
+      }
+
     // @todo
     ""
   }
 }
 
 object Candidate {
-  def randomCandidate(groups: Seq[Group], activities: Seq[Activity]): Candidate = {
-    // @todo
-    Candidate(Map())
-  }
+  def randomCandidate(groups: Seq[Group], activities: Seq[Activity]): Candidate =
+    Candidate(Map(groups.map(_ -> Activity.randomOrder(activities)): _*))
 }
